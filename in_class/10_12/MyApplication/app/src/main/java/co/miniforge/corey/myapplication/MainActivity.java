15710f7fe4b87d3b99cf25ea69b36daa1a26e7ec
Corey Johnson <cor.johnson@me.com>
2017-10-12 19:51:31 -0700

    shape

--- in_class/10_12/MyApplication/app/src/main/java/co/miniforge/corey/myapplication/MainActivity.java
+++ in_class/10_12/MyApplication/app/src/main/java/co/miniforge/corey/myapplication/MainActivity.java
@@ -2,10 +2,14 @@ package co.miniforge.corey.myapplication;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.openURL);
+        final Button button = (Button) findViewById(R.id.openURL);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,12 +28,28 @@ public class MainActivity extends AppCompatActivity {
 //                i.setData(Uri.parse(url));
 //                startActivity(i);
 
-                String message = "Hello!";
+                /*String message = "Hello!";
                 Intent i2 = new Intent(Intent.ACTION_SEND);
                 i2.putExtra(Intent.EXTRA_TEXT, message);
                 i2.setType("text/plain");
-                startActivity(i2);
+                startActivity(i2);*/
+
+                /* Requires compile 'com.android.support:design:26.+' in the app build.gradle */
+                Snackbar.make(button, "My snackbar", Snackbar.LENGTH_SHORT).show();
             }
         });
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        String url = "https://image.freepik.com/free-psd/white-mobile-phone-mock-up_1104-79.jpg";
+        Glide.with(this).load(url).into(imageView);
     }
 }
+
+
+
+
+
+
+
+
