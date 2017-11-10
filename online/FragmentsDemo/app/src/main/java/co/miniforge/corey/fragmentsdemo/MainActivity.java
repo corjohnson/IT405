69fac5b74b6f4d92243f29ffb69849710aaac72c
Corey Johnson <cor.johnson@me.com>
2017-11-09 18:12:41 -0800

    FragmentDemo

---
+++ online/FragmentsDemo/app/src/main/java/co/miniforge/corey/fragmentsdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package co.miniforge.corey.fragmentsdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by corey on 11/9/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
+
